1e4bf35f27139a1881d15ae22ae53903212cdb39
Alyssa Clancy <aclancy93@gmail.com>
2018-10-07 12:55:23 -0500

    Commit includes updated comment with overall summary of how to transpile code to add clarity.

--- Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Build.js
+++ Code-Academy-Exercises/Introduction-To-JavaScript/Browser-Compatibility-and-Transpilation/Build.js
@@ -11,4 +11,13 @@ var carbonara = "You can make carbonara with " + pasta + ", " + meat + ", and a 
 
 /*I previously added a "build" script to package.json (updated the Babel-Soure-Lib.json file to .json extension).
 Now, running npm run build enables the code to be transpiled. The new file of transpiled code is copied above.
+In total, the steps for setting a project up for transpilation include:
+1. initialize project using npm init and create a directory called src.
+2. install babel dependencies by running npm install babel-cli -D and npm install babel-preset-env -D
+3. create a .babelrc file inside your project with the code
+ {
+ "presets": ["env"]
+ }
+4. add "build": "babel src -d lib" to scripts object in package.json
+5. npm run build when you want to transpile code from src to lib directory.
 */
